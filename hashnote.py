d6addbc65cb919e8dbcd37707994b0f74f8d63b2
Thibault <thibault.barbie@free.fr>
2023-06-07 01:30:31 +0900

    Save data in a csv file with an attached hex

--- hashnote.py
+++ hashnote.py
@@ -1,9 +1,21 @@
 import argparse
+import csv
+import os
 
 if __name__== "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--recall", type=ascii)
+    parser.add_argument("-r", "--recall", help="Recall the note from the hash.", type=ascii)
     
     args, unknown = parser.parse_known_args()
-    print(unknown)
-    print(args)
+    args = vars(args)
+
+    if len(unknown)>0:
+        if os.path.isfile('notes.csv'): 
+            num_lines = sum(1 for _ in open('notes.csv'))
+        else:
+            num_lines = 0
+        with open('notes.csv', 'a', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            
+            for i, note in enumerate(unknown):
+                spamwriter.writerow([f'{num_lines+i:x}', note])
